0d1c9b73c256d32668f9653b2e65e80818f7393d
Joel Barkley <joel.barkley@colorado.edu>
2020-04-18 16:24:24 -0600

    changed menu design

--- Assets/Scripts/WhaleStats.cs
+++ Assets/Scripts/WhaleStats.cs
@@ -4,27 +4,5 @@ using UnityEngine;
 
 public class WhaleStats : MonoBehaviour
 {
-    IHealth wHealth;
-    public float blood;
-    public float armor;
-    public float totalWhaleHealth = wHealth.Health.get() * blood * armor;
 
-    public float fireRate; // projectils per second
-    public float projectileDmg; // damage of projectile
-    
-    void Start()
-    {
-
-    }
-
-    void Update()
-    {
-        TakeDamage();
-    }
-
-
-    public void TakeDamage(float amount)
-    {
-        return totalWhaleHealth - amount;
-    }
 }
