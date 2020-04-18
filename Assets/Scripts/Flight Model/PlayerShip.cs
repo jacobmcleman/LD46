53db1cfea62b30843d2c6d940281d8f123bdf4eb
Jake McLeman <aevek@vaguelydamp.com>
2020-04-18 14:10:33 -0700

    Started breaking out ship controls into another file

---
+++ Assets/Scripts/Flight Model/PlayerShip.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpaceshipController))]
+public class PlayerShip : MonoBehaviour
+{
+    public UnityEngine.UI.Slider throttleIndicator;
+    public UnityEngine.UI.Slider speedIndicator;
+    public UnityEngine.UI.Text speedText;
+    public GameObject mouseUI;
+
+    private SpaceshipController stick;
+
+    void Start()
+    {
+        stick = GetComponent<SpaceshipController>();
+    }
+
+    void Update()
+    {
+        HandleThrottleInput();
+        HandleRotationInput();
+    }
+
+    private void HandleThrottleInput()
+    {
+        stick.ThrottleInput = Input.GetAxis("Throttle");
+    }
+
+
+    private void HandleRotationInput()
+    {
+
+    }
+}
