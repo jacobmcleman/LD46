8b00d5b6c1aa2e24304cf94e8da59357aeb6a079
Eli <elijw22@gmail.com>
2020-04-18 01:28:55 -0400

    player health

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour, IHealth
+{
+    public float MaxHealth { get; set; }
+    public float Health { get { return health; } }
+    private float health;
+
+    public void TakeDamage(float damage)
+    {
+        if (health - damage <= 0)
+        {
+            //Die
+        }
+        else
+        {
+            health -= damage;
+        }
+    }
+
+    public void Heal(float healAmount)
+    {
+        if (health + healAmount >= MaxHealth)
+        {
+            health = MaxHealth;
+        }
+        else
+        {
+            health += healAmount;
+        }
+    }
+
+    void Awake()
+    {
+        health = MaxHealth;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
