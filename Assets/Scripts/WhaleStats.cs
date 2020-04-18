b2c4f109fe41686845a49a60f6ffde76fdfa8df4
Joel Barkley <joel.barkley@colorado.edu>
2020-04-18 16:54:14 -0600

    Update WhaleStats.cs

--- Assets/Scripts/WhaleStats.cs
+++ Assets/Scripts/WhaleStats.cs
@@ -4,5 +4,28 @@ using UnityEngine;
 
 public class WhaleStats : MonoBehaviour
 {
+    //    IHealth wHealth;
+    public float blood;
+    public float armor;
+    //    public float totalWhaleHealth = wHealth.Health.get() * blood * armor;
+
+    public float fireRate; // projectils per second
+    public float projectileDmg; // damage of projectile
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        //        TakeDamage();
+    }
+
+
+    public void TakeDamage(float amount)
+    {
+        //        return totalWhaleHealth - amount;
+    }
 
 }
