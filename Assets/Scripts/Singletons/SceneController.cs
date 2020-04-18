78ffebcb8a283a73c2b3b81f6f065e4009ccf73d
JEITech <chris@james-enterprise.com>
2020-04-17 23:02:00 -0600

    UPdated to latest Unity, and also, added Splash screen and menu stuff

---
+++ Assets/Scripts/Singletons/SceneController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{ 
+    //*
+    //// Fields
+    //*
+
+    //Singleton isntance
+    public static SceneController instance { get; private set; }
+
+    //*
+    //// Unity Methods
+    //*
+
+    //On awake, assign the singleton instance
+    private void Awake ()
+    {
+        //Instance should be null, we only want one instance
+        if (instance == null)
+        {
+            //Set the instance to this and trigger switch to main menu from startup scene
+            instance = this;
+            //Don't destroy the game object when we load new scenes
+            DontDestroyOnLoad(this.gameObject);
+            StartCoroutine(InitMenu());
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    //*
+    //// Public Methods
+    //*
+
+    //Change scene to specified string 
+    public void ChangeScene (string newScene)
+    {
+        SceneManager.LoadScene(newScene);
+    }
+
+    //*
+    //// Private Methods
+    //*
+
+    //Called on startup after singleton instance is set
+    private IEnumerator InitMenu ()
+    {
+        //Wait for splash screen video to play
+        yield return new WaitForSeconds(7);
+        ChangeScene("Main Menu");
+    }
+}
