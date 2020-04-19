ac604c79914787b8c1e1d8fea38316e8cb9bb65e
Eli <elijw22@gmail.com>
2020-04-19 18:13:37 -0400

    Nailed the whale rail bail (#11)

---
+++ Assets/Scripts/Whale/WhaleRail.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhaleRail : MonoBehaviour
+{
+    public bool doGizmo = true;
+
+    [SerializeField]
+    public Transform[] RailPoints;
+
+    private void Start()
+    {
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (!doGizmo) return;
+
+        Gizmos.color = Color.green;
+
+        for (int i = 0; i < RailPoints.Length; i++)
+        {
+            Gizmos.DrawSphere(RailPoints[i].position, 10f);
+
+            if (i > 0)
+            {
+                Gizmos.DrawLine(RailPoints[i - 1].position, RailPoints[i].position);
+            }
+        }
+    }
+}
