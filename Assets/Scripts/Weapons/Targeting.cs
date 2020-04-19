15b5a46766929a4f53f9c46e8688fffb4a3faad9
Eli <elijw22@gmail.com>
2020-04-19 19:00:20 -0400

    Targeting System in

--- Assets/Scripts/Weapons/Targeting.cs
+++ Assets/Scripts/Weapons/Targeting.cs
@@ -6,5 +6,57 @@ public class Targeting : MonoBehaviour
 {
     public Transform Target;
 
+    public string TargetTag = "Enemy";
 
+    private GameObject[] enemies;
+    private int enemyIndex = 0;
+
+    private void Start()
+    {
+        enemies = GameObject.FindGameObjectsWithTag(TargetTag);
+        Target = enemies[0].transform;
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("GetBestTarget"))
+        {
+            Target = GetBestTarget();
+        }
+        if (Input.GetButtonDown("GetNextTarget"))
+        {
+            Target = GetNextTarget();
+        }
+        if (Target == null)
+        {
+            GetNextTarget();
+        }
+    }
+
+    private Transform GetBestTarget()
+    {
+        enemies = GameObject.FindGameObjectsWithTag(TargetTag);
+        float bestTargetVal = float.MinValue;
+        Transform bestTarget = null;
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            float forwardNess = Vector3.Dot((enemies[i].transform.position - transform.position).normalized, transform.forward);
+            if (forwardNess > bestTargetVal)
+            {
+                bestTargetVal = forwardNess;
+                bestTarget = enemies[i].transform;
+                enemyIndex = i;
+            }
+        }
+
+        return bestTarget;
+    }
+
+    private Transform GetNextTarget()
+    {
+        if (enemyIndex < enemies.Length) { enemyIndex++; }
+        else { enemyIndex = 0; }
+
+        return enemies[enemyIndex].transform;
+    }
 }
