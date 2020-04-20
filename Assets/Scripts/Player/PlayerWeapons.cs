b95d271a3e6b012b0a3971bdb597f2ca32c914a4
keenanwulffcolo <keenan.wulff@colorado.edu>
2020-04-19 21:19:27 -0600

    added the other gun

--- Assets/Scripts/Player/PlayerWeapons.cs
+++ Assets/Scripts/Player/PlayerWeapons.cs
@@ -15,11 +15,15 @@ public class PlayerWeapons : MonoBehaviour, IWieldable
     }
     public GameObject weapon1;
     private IFireable weaponScript1;
+    public GameObject weapon2;
+    private IFireable weaponScript2;
     // Start is called before the first frame update
     void Start()
     {
         weaponScript1 = weapon1.GetComponent<IFireable>();
         weaponScript1.team = team;
+        weaponScript2 = weapon2.GetComponent<IFireable>();
+        weaponScript2.team = team;
     }
 
     // Update is called once per frame
@@ -28,7 +32,7 @@ public class PlayerWeapons : MonoBehaviour, IWieldable
         if (Input.GetButton("Fire1"))
         {
             weaponScript1.Fire(this);
+            weaponScript2.Fire(this);
         }
-        
     }
 }
