511cf797e91d1e4b7c1d80e4b24e9f259feb59f3
JEITech <chris@james-enterprise.com>
2020-04-19 23:12:03 -0600

    shit

--- Assets/Scripts/TutoralPopUps.cs
+++ Assets/Scripts/TutoralPopUps.cs
@@ -18,7 +18,7 @@ public class TutoralPopUps : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
