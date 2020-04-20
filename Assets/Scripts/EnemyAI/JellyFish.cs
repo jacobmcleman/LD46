b85331fe0e4fa7dda7d159830569e94467d1b2b8
drsnowdude <sanctum6780@gmail.com>
2020-04-19 18:25:01 -0600

    added JellyFish whale tracking

--- Assets/Scripts/EnemyAI/JellyFish.cs
+++ Assets/Scripts/EnemyAI/JellyFish.cs
@@ -2,17 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+[RequireComponent(typeof(AIShip))]
+public class JellyFish : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform whalePosition;
+
+    private AIShip jellyAI;
+
     void Start()
     {
-        
+        whalePosition = GameObject.FindGameObjectWithTag("Whale").transform; 
+        jellyAI = GetComponent<AIShip>();
+        jellyAI.TargetPosition = whalePosition.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        whalePosition = GameObject.FindGameObjectWithTag("Whale").transform; //find Whale position
+        jellyAI.TargetPosition = whalePosition.position;
+        Debug.Log("Jellyfish Position" + transform.position);
     }
 }
